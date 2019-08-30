bff82fe77e5ff7d71012c9908865f621e61ec00e
Bhasker Yadav <49790723+bhashkaryadav@users.noreply.github.com>
2019-08-30 09:31:28 +0530

    Update Calci.java

--- Calci.java
+++ Calci.java
@@ -1,29 +1,6 @@
-interface Calc
-{
-	float add(int a,int b);
-	float divide(int x,int y);
-	float subtract(int c ,int d);
-	float multiply(int s,int t);
+public class DevopsAttendance{
+public static void main(String arr[]){
+System.out.println("My name is Bhashkar yadav Reg NO. 11703318");
+
 }
-class Calci implements Calc 
-{
-	public  float add(int a,int b){
-		return (a+b);
-	}
-	public  float divide(int x ,int y){
-		return (x/y);
-	}
-	public  float subtract(int c ,int d){
-		return (c-d);
-	}
-	public  float multiply(int s ,int t){
-		return (s*t);
-	}
-	public static void main(String []arr){
-	 Calci obj = new Calci();
-	 System.out.println(obj.add(3,4));
-	 System.out.println(obj.multiply(5,4));
-	 System.out.println(obj.subtract(7,4));
-	 System.out.println(obj.divide(67,4));
-	}
 }
